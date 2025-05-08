04682a8bcec94d11d2376fe62a4c7032faa31d5b
saketh <sakethsunny216@gmail.com>
2025-05-08 15:55:13 +0530

    java Docker test

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@ public class Sample
 {
         public static void main(String args[])
         {
-System.out.println("Vengala Saketh");
+               System.out.println("Vengala Saketh");
         }
 }
